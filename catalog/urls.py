50f676e1f4f783a9f627fbe991ca07d398ea7a6f
esmrzv <esmurziev.1999@mail.ru>
2024-06-15 20:04:38 +0300

    добавлены шаблоны и все остальное

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from catalog.views import home, contacts
+
+from catalog.apps import CatalogConfig
+from catalog.views import home, contacts, product_detail
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
 
-    path('', home),
-    path('contacts/', contacts)
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('product/<int:pk>/', product_detail, name='product_detail')
 
 ]
