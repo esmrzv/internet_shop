bcbc4634541f67a8cb13266d850e58807bbdba37
esmrzv <esmurziev.1999@mail.ru>
2024-07-16 01:01:42 +0300

    реализована регистрация по почте

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,11 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
+from users.views import UserRegisterView
 
 app_name = UsersConfig.name
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', UserRegisterView.as_view(), name='register'),
     ]
